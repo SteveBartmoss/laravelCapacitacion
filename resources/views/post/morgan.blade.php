7296d97e51387fac5b2ac0a9eee5907b8ce8752f
Marco Antonio Guadalupe Rivas Guzman <marco.rivas@carsol.mx>
2025-07-10 17:15:16 -0600

    feat: Se implemento una vista para mostrar el post sobre morgan blackhand

---
+++ resources/views/post/morgan.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts/main')
+
+@section('content')
+<h1>Morgan Black Hand</h1>
+<p>El unico solo</p>
+@endsection
+
+@section('morecontent')
+<h1>Logros</h1>
+<p>Ser morgan black hand</p>
+@endsection
