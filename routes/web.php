461841ada0ab6c962908d7aec8a4d379bd25acbd
Marco Antonio Guadalupe Rivas Guzman <marco.rivas@carsol.mx>
2025-07-10 10:59:14 -0600

    feat: Se agregaron rutas para la leccion de las vistas y como navegar entre pagina del a aplicacion

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/test', function (){
+    return view('test');
+});
+
+Route::get('/crud', function(){
+    $data = ['name'=> 'Raichu', 'age' => 22];
+    return view('crud/index',$data);
+})->name('crud');
