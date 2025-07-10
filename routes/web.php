3df8bb58b36092b7d9ec67c2c0695b7be92edb9c
Marco Antonio Guadalupe Rivas Guzman <marco.rivas@carsol.mx>
2025-07-10 13:14:29 -0600

    feat: Se implemento la redireccion en la ruta de contact1 a contacto2

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,14 @@ Route::get('/crud', function(){
     $data = ['name'=> 'Raichu', 'age' => 22];
     return view('crud/index',$data);
 })->name('crud');
+
+Route::get('/contact1', function(){
+
+    return redirect()->route('contact2');
+    return to_route('contact2');
+    
+})->name('contact1');
+
+Route::get('/contact2', function(){
+    return view('contact/contact2');
+})->name('contact2');
