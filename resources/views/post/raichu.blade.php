9716af23b2f6b8ba918aef0990712e1dcc0725ba
Marco Antonio Guadalupe Rivas Guzman <marco.rivas@carsol.mx>
2025-07-10 16:27:52 -0600

    feat: Se implemento un layout para la practica del curso de laravel

---
+++ resources/views/post/raichu.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts/main')
+
+@section('content')
+<h1>Never fade away</h1>
+<p>We lost everything </p>
+@endsection
+
+@section('morecontent')
+<h1>Jonhy Silverhand</h1>
+<p>Silverhand xd</p>
+@endsection
