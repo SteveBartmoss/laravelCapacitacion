28986a69dbf69d687b409b01a8ba57b915a9a709
Marco Antonio Guadalupe Rivas Guzman <marco.rivas@carsol.mx>
2025-07-15 10:38:53 -0600

    feat: Se creo la vista del formulario para crear un post

--- resources/views/dashboard/post/create.blade.php
+++ resources/views/dashboard/post/create.blade.php
@@ -6,22 +6,22 @@
         <label for="">Title</label>
         <input type="text" name="title">
         <label for="">Slug</label>
+        <input type="text" name="slug">
+        <label for="">Content</label>
+        <input type="text" name="content">
         <label for="">Category</label>
         <select name="category">
             @foreach ($categories as $title => $id)
                 <option value="{{$id}}">{{$title}}</option>
             @endforeach
         </select>
-        <input type="text" name="slug">
         <label for="">Description</label>
+        <input type="text" name="description">
         <label for="">Posted</label>
         <select name="posted">
-            <option value="yes">Yes</option>
             <option value="not">Not</option>
+            <option value="yes">Yes</option>
         </select>
-        <input type="text" name="description">
-        <label for="">Content</label>
-        <input type="text" name="content">
         <label for="">Image</label>
         <input type="text" name="image">
         <button type="submit">Send</button>
