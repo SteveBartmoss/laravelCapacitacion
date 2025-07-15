31a93dde9489703ae475c1260cf948133ec71e70
Marco Antonio Guadalupe Rivas Guzman <marco.rivas@carsol.mx>
2025-07-15 16:54:18 -0600

    feat: Se agregaron los elementos para la edicion de informacion

--- resources/views/dashboard/post/list.blade.php
+++ resources/views/dashboard/post/list.blade.php
@@ -3,22 +3,50 @@
 @section('content')
     <h1>Lista de post creados</h1>
 
+    <h1>Puedes crear un post en el siguiente enlace</h1>
+    <a href="{{route('post.create')}}">Crear post</a>
+
     <table>
         <thead>
-            <tr>Title</tr>
-            <tr>Posted</tr>
-            <tr>Category</tr>
+            <tr>
+                <td>
+                    id
+                </td>
+                <td>
+                    Title
+                </td>
+                <td>
+                    Posted
+                </td>
+                <td>
+                    Category
+                </td>
+                <td>
+                    options
+                </td>
+            </tr>
+           
         </thead>
         <tbody>
             @foreach ($postList as $key => $value)
             <tr>
+                <td>
+                    {{ $value->id }}
+                </td>
                 <td>
                     {{$value->title}}
                 </td>
                 <td>
                     {{$value->posted}}
                 </td>
-                <td>{{$value->category->title}}</td>
+                <td>
+                    {{$value->category->title}}
+                </td>
+                <td>
+                    <a href="{{route('post.edit',$value->id)}}">Editar</a>
+                    <a href="{{route('post.show',$value->id)}}">ver</a>
+                    <a href="{{route('post.destroy',$value->id)}}">Eliminar</a>
+                </td>
             </tr>
             @endforeach
         </tbody>
