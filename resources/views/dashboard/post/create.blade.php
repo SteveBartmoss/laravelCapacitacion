a84d7a25dee1175d371a1bc1053bc361b2e86682
Marco Antonio Guadalupe Rivas Guzman <marco.rivas@carsol.mx>
2025-07-16 10:54:34 -0600

    feat: Se implemento un textarea en lugar del imput

--- resources/views/dashboard/post/create.blade.php
+++ resources/views/dashboard/post/create.blade.php
@@ -49,7 +49,7 @@
 
                 <div>
                     <label for="description" class="block text-sm font-medium text-gray-700 mb-1">Description</label>
-                    <input type="text" name="description" class="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500">
+                    <textarea type="text" name="description" class="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"></textarea>
                 </div>
 
                 <div>
